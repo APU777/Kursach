1133cfc53def602da601508c9411cc98d816fec5
Vitaliy Alexandrovich <Dobrota2017@outlook.com>
2018-11-05 00:09:18 +0200

    Initial commit

---
+++ Kursach/Logic_sAlgorithms.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Kursach
+{
+    internal static class Logic_sAlgorithms
+    {
+        public static void MixCards(ref float[] _Array)
+        {
+            Random random = new Random();
+            List<string> R_Arr = new List<string>();
+            Queue<float> ReturnQueue = new Queue<float>();
+            int CardIndex = 0;
+            try
+            {
+                while (R_Arr.Count < _Array.Length)
+                {
+                    CardIndex = random.Next(0, _Array.Length);
+                    if (!R_Arr.Contains(CardIndex.ToString()))
+                    {
+                        R_Arr.Add(CardIndex.ToString());
+                        ReturnQueue.Enqueue(_Array[CardIndex]);
+                    }
+                }
+
+                for(int i = 0; i < _Array.Length; ++i)
+                    _Array[i] = ReturnQueue.Dequeue();
+            }
+            catch(Exception Exc)
+            {
+                MessageBox.Show(Exc.Message);
+            }
+        }
+
+        public static void LayingCards (ref float[] _Array, ref Queue<float> To_Store)
+        {
+            foreach (float _Arr in _Array)
+                To_Store.Enqueue(_Arr);
+        }
+
+        public static int _TrumpCard(ref float[] _Array)
+        {
+            try
+            {
+                Random random = new Random();
+                return (int)_Array[random.Next(0, _Array.Length)];
+            }
+            catch (Exception Exc)
+            {
+                MessageBox.Show(Exc.Message);
+            }
+            return (int)_Array[7];
+        }
+
+        public static List<float> DistributeCards(ref List<float> Pl_Cards, ref Queue<float> CardsStore)
+        {
+           while(Pl_Cards.Count < 6)
+                Pl_Cards.Add(CardsStore.Dequeue());
+
+           return Pl_Cards.OrderBy(OBfMnToMx => OBfMnToMx).ToList();
+        }
+
+        public static void LookCards(ref List<float> Pl_Cards, ref Label[] Pl_CardsImage)
+        {
+            for (int index = 0; index < Pl_CardsImage.Length; ++index)
+            {
+
+                Pl_CardsImage[index].Background = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/Kursach;component/CardsImage/"+ Pl_Cards[index] +".png")));
+            }
+        }
+    }
+}
---
+++ Kursach/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kursach
+{
+
+    public partial class MainWindow : Window
+    {
+        private static byte QuantityPlayers = 2;
+        private static float[] BigCardsArray = new float[36] { 1.1f, 1.2f, 1.3f, 1.4f, 2.1f, 2.2f, 2.3f, 2.4f, 3.1f, 3.2f, 3.3f, 3.4f, 4.1f, 4.2f, 4.3f, 4.4f, 5.1f, 5.2f, 5.3f, 5.4f, 6.1f, 6.2f, 6.3f, 6.4f, 7.1f, 7.2f, 7.3f, 7.4f, 8.1f, 8.2f, 8.3f, 8.4f, 9.1f, 9.2f, 9.3f, 9.4f };
+        private static Queue<float> CardsStore = new Queue<float>();
+        private static Label[] PlayerCardsImage = null;
+        private static List<float>[] PlayersCards = new List<float>[4];
+
+        private void InitializePlayers()
+        {
+            PlayerCardsImage = new Label[6] { Card_One, Card_Two, Card_Three, Card_Four, Card_Five, Card_Six };
+
+            for(int index = 0; index < QuantityPlayers; ++index)
+                PlayersCards[index] = new List<float>();
+
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializePlayers();
+            Logic_sAlgorithms.MixCards(ref BigCardsArray);
+            Logic_sAlgorithms.LayingCards(ref BigCardsArray, ref CardsStore);
+            PlayersCards[0] = Logic_sAlgorithms.DistributeCards(ref PlayersCards[0], ref CardsStore);
+            Logic_sAlgorithms.LookCards(ref PlayersCards[0], ref PlayerCardsImage);
+
+
+        }
+    }
+}
